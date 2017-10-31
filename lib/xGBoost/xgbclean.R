c9959bcd4c253f27e9e4d17b2e1c5a2bf5482daa
Jordan Leung <giordun@dyn-160-39-160-53.dyn.columbia.edu>
2017-10-31 15:35:34 -0400

    clean xgb

--- lib/xGBoost/xgbclean.R
+++ lib/xGBoost/xgbclean.R
@@ -3,9 +3,9 @@ library(readr)
 library(stringr)
 library(caret)
 
-# setwd("~/ADS/Fall2017-project3-fall2017-project3-grp8")
+setwd("~/ADS/Fall2017-project3-fall2017-project3-grp8")
 
-# labs <- read_csv("data/our_data/training_set/label_train.csv")
+labs <- read_csv("data/our_data/training_set/label_train.csv")
 
 labs[,3] <- 0
 labs[,3][labs[,2] == 0,] <- 'A'
@@ -46,108 +46,14 @@ model1 <- train(x = train[, -c(1,2)], y = train[,2], method='xgbTree', trControl
 preds <- predict(model1, test[, -c(1,2)])
 acc <- 1 - sum(preds != test[,2])/length(test[,2])
 
-# The final values used for the model were nrounds = 100, max_depth = 10, eta = 0.1, gamma = 1,
-# colsample_bytree = 1, min_child_weight = 1 and subsample = 0.5.
-# CV Accuracy 0.784, Test Acc 0.768
-
-xgb_grid_2 = expand.grid(
-  nrounds = 100,
-  max_depth = c(8, 10, 12),
-  eta = c(0.1, 0.2),
-  gamma = c(0.5, 1, 3),
-  colsample_bytree = c(1),
-  min_child_weight = c(1, 3, 5),
-  subsample = c(0.4, 0.5, 0.6)
-)
-
-model2 <- train(x = train[, -c(1,2)], y = train[,2], method='xgbTree', trControl=control, tuneGrid = xgb_grid_2, objecitve = "multi:softprob", tuneLength=3)
-
-# Fitting nrounds = 100, max_depth = 12, eta = 0.2, gamma = 0.5, colsample_bytree = 1, min_child_weight = 5, subsample = 0.4 on full training set
-# CV Accuracy 0.2075599, Test Acc 0.238667
-
-preds <- predict(model2, test[, -c(1,2)])
-acc <- 1 - sum(preds != test[,2])/length(test[,2])
-
-xgb_grid_3 = expand.grid(
-  nrounds = 100,
-  max_depth = c(12, 14),
-  eta = c(0.1, 0.15, 0.2),
-  gamma = c(0.25, 0.5, 0.75),
-  colsample_bytree = c(1),
-  min_child_weight = c(4, 5, 6),
-  subsample = c(0.4, 0.45, 0.5)
-)
-
-model3 <- train(x = train[, -c(1,2)], y = train[,2], method='xgbTree', trControl=control, tuneGrid = xgb_grid_3, objecitve = "multi:softprob", tuneLength=3)
-
-1 - max(model3[["results"]][["Accuracy"]])
-
-preds <- predict(model3, test[, -c(1,2)])
-err <- sum(preds != test[,2])/length(test[,2])
-
-# Fitting nrounds = 100, max_depth = 14, eta = 0.15, gamma = 0.5, colsample_bytree = 1, min_child_weight = 6, subsample = 0.5 on full training set
-
-# CV err 0.2111046, err = 0.2306667
-
-xgb_grid_4 = expand.grid(
-  nrounds = 100,
-  max_depth = c(13, 14, 15),
-  eta = c(0.15, 10),
-  gamma = c(0.5, 20),
-  colsample_bytree = c(1),
-  min_child_weight = c(6, 12),
-  subsample = c(0.5, 0.7)
-)
-
-model4 <- train(x = train[, -c(1,2)], y = train[,2], method='xgbTree', trControl=control, tuneGrid = xgb_grid_4, objecitve = "multi:softprob", tuneLength=3)
-
-preds <- predict(model4, test[, -c(1,2)])
-err <- sum(preds != test[,2])/length(test[,2])
-
-# Fitting nrounds = 100, max_depth = 15, eta = 0.15, gamma = 0.5, colsample_bytree = 1, min_child_weight = 6, subsample = 0.7 on full training set
-
-#CV err 0.2186661, err = 0.22
-
-xgb_grid_5 = expand.grid(
-  nrounds = 100,
-  max_depth = c(14, 15, 16),
-  eta = c(0.13, 0.15, 0.17),
-  gamma = c(0.4, 0.5, 0.6),
-  colsample_bytree = c(1),
-  min_child_weight = c(5.5, 6, 6.5),
-  subsample = c(0.5, 0.6, 0.7)
-)
-
-model5 <- train(x = train[, -c(1,2)], y = train[,2], method='xgbTree', trControl=control, tuneGrid = xgb_grid_5, objecitve = "multi:softprob", tuneLength=3)
-
-1 - max(model5[["results"]][["Accuracy"]])
-
-preds <- predict(model5, test[, -c(1,2)])
-err <- sum(preds != test[,2])/length(test[,2])
-
-# nrounds = 100, max_depth = 16, eta = 0.17, gamma = 0.5, colsample_bytree = 1, min_child_weight = 6.5, subsample = 0.6 on full training set
-# CV Error  0.2097821, test err = 0.2093333
-
-xgb_grid_6 = expand.grid(
-  nrounds = 100,
-  max_depth = c(15, 16, 17),
-  eta = c(0.16, 0.17, 0.18),
-  gamma = c(0.45, 0.5, 0.55),
-  colsample_bytree = c(1),
-  min_child_weight = c(6.3, 6.5, 6.7),
-  subsample = c(0.55, 0.6, 0.65)
-)
-
-model6 <- train(x = train[, -c(1,2)], y = train[,2], method='xgbTree', trControl=control, tuneGrid = xgb_grid_6, objecitve = "multi:softprob", tuneLength=3)
-
-preds <- predict(model6, test[, -c(1,2)])
-err <- sum(preds != test[,2])/length(test[,2])
-
-# max_depth = 17, eta = 0.18, gamma = 0.45, colsample_bytree = 1, min_child_weight = 6.5, subsample = 0.65 on full training set
-
-# CV Accuracy - 0.2093459, test err = 0.2186667
-
-# might be starting to overfit here
+# Tune 1: CV Error 0.216, Test Error 0.232
+# Tune 2: CV Error 0.2075599, Test Error 0.238667
+# Tune 3: CV Error 0.2111046, Test Error 0.2306667
+# Tune 4: CV Error 0.2186661, Test Error 0.22
+# Tune 5: CV Error  0.2097821, Test Error = 0.2093333
+# Tune 6: CV Error 0.2093459, Test err = 0.2186667
+### might be starting to overfit here
+# Tune 7: CV err = 0.2084235, Test err = 0.2146667
 
 xgb_grid_7 = expand.grid(
   nrounds = 100,
@@ -165,13 +71,8 @@ model7 <- train(x = train[, -c(1,2)], y = train[,2], method='xgbTree', trControl
 preds <- predict(model7, test[, -c(1,2)])
 err <- sum(preds != test[,2])/length(test[,2])
 
-# max_depth = 18, eta = 0.18, gamma = 0.45, colsample_bytree = 1, min_child_weight = 6.6, subsample = 0.7 on full training set
-# cv err = 0.2084235, test err = 0.2146667
-
 # Best model is model 5
-
-# nrounds = 100, max_depth = 16, eta = 0.17, gamma = 0.5, colsample_bytree = 1, min_child_weight = 6.5, subsample = 0.6 on full training set
-
+# max_depth = 18, eta = 0.18, gamma = 0.45, colsample_bytree = 1, min_child_weight = 6.6, subsample = 0.7 on full training set
 
 xgbbest <- xgboost(data = data.matrix(train[,-c(1,2)]),
                       label = train[,1],
@@ -186,7 +87,7 @@ xgbbest <- xgboost(data = data.matrix(train[,-c(1,2)]),
                       early_stopping_rounds = 100,
                       prediction = TRUE,
                       metrics = "merror",
-                      objective = "multi:softmax",)
+                      objective = "multi:softmax")
 
 preds <- predict(xgbbest, data.matrix(test[, -c(1,2)]))
 err <- sum(preds != test[,1])/length(test[,1])
