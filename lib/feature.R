9d44c5ff1156a74cb6152f7180ecd5b88abef632
duanshiqi <duanshiqi@users.noreply.github.com>
2017-10-30 21:37:52 -0400

    our feature.R for HOG

--- lib/feature.R
+++ lib/feature.R
@@ -1,39 +1,29 @@
-#############################################################
-### Construct visual features for training/testing images ###
-#############################################################
-
-### Authors: Yuting Ma/Tian Zheng
-### Project 3
-### ADS Spring 2017
-
-feature <- function(img_dir, set_name, data_name="data", export=T){
+feature<-function(img_dir, export=TRUE){
   
   ### Construct process features for training/testing images
-  ### Sample simple feature: Extract row average raw pixel values as features
+  ### HOG: calculate the Histogram of Oriented Gradient for an image
   
   ### Input: a directory that contains images ready for processing
   ### Output: an .RData file contains processed features for the images
   
+  
   ### load libraries
   library("EBImage")
+  library("OpenImageR")
   
-  n_files <- length(list.files(img_dir))
+  dir_names <- list.files(img_dir)
+  n_files <- length(dir_names)
   
-  ### determine img dimensions
-  img0 <-  readImage(paste0(img_dir, "img", "_", data_name, "_", set_name, "_", 1, ".jpg"))
-  mat1 <- as.matrix(img0)
-  n_r <- nrow(img0)
-  
-  ### store vectorized pixel values of images
-  dat <- matrix(NA, n_files, n_r) 
+  ### calculate HOG of images
+  dat <- matrix(NA, n_files, 54) 
   for(i in 1:n_files){
-    img <- readImage(paste0(img_dir,  "img", "_", data_name, "_", set_name, "_", i, ".jpg"))
-    dat[i,] <- rowMeans(img)
+    img <- readImage(paste0(img_dir,  dir_names[i]))
+    dat[i,] <- HOG(img)
   }
   
   ### output constructed features
   if(export){
-    save(dat, file=paste0("../output/feature_", data_name, "_", set_name, ".RData"))
+    save(dat, file=paste0("../output/HOG.RData"))
   }
   return(dat)
 }
